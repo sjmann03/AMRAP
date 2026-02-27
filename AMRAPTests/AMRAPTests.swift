0151cc5f21dd4347d6f4834dcbbd184bc1e37787
S <sjmann03@gmail.com>
2026-02-26 22:58:15 -0500

    Initial Commit

---
+++ AMRAPTests/AMRAPTests.swift
@@ -0,0 +1,16 @@
+//
+//  AMRAPTests.swift
+//  AMRAPTests
+//
+//  Created by Spencer Mann on 2/26/26.
+//
+
+import Testing
+
+struct AMRAPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
